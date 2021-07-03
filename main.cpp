9840e8462d8dba43a0223a376f7afb824db2e3a0
Sai Sivakumar <sivakumarsai27@gmail.com>
2021-07-03 16:01:05 -0400

    apparently I have to commit some changes before I checkout your branch

--- main.cpp
+++ main.cpp
@@ -3,10 +3,5 @@
 #include "Parser.h"
 
 int main() {
-	std::string expression = "( 3 + 2 ) * 5";
-	std::vector<std::string> tokens;
-	Parser::parse(expression, tokens);
-	for (int i = 0; i < tokens.size(); i++) {
-		std::cout << tokens[i] << std::endl;
-	}
+	
 }
