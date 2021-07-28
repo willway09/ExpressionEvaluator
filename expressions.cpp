525c357cf8765a9d71574e0c0c4bbcdf992e2bd3
Will <willway@outlook.com>
2021-07-28 18:11:38 -0400

    Batch testing works, single-number expressions are rare, removed output from StackEval

--- expressions.cpp
+++ expressions.cpp
@@ -3,6 +3,7 @@
 #include <ctime>
 #include <string>
 #include <fstream>
+#include <string>
 
 char operators[] = {
 	'+',
@@ -11,10 +12,18 @@ char operators[] = {
 	'/'
 };
 
-std::string generateExpression(int level) {
-	int count = std::rand() % 5; // Maximum of 5 operators, between 0 and 4
+std::string generateExpression(int level, int baseLevel) {
+	int count = std::rand() % 5; // Maximum of 5 operands, between 0 and 4
 
 	if(level == 0) count = 0; //Cap recursion at level
+
+	if(count == 0 && baseLevel == level) {
+		if(std::rand() % 1000 == 0) { //1 in 1000 chance of being a single-number expression
+			std::cout << "Here" << std::endl;
+		} else { //Otherwise
+			count = std::rand() % 4 + 1; // Maximum of 5 operands, between 1 and 4
+		}
+	}
 
 	if(count == 0) {
 		double num = std::rand();
@@ -25,7 +34,7 @@ std::string generateExpression(int level) {
 
 		for(int i = 0; i < count; i++) {
 
-			rtn += generateExpression(level - 1);	
+			rtn += generateExpression(level - 1, baseLevel);	
 
 			if(i != count - 1) { //If not last element
 				rtn += operators[std::rand() % 4];
@@ -38,15 +47,19 @@ std::string generateExpression(int level) {
 	}
 }
 
-int main() {
+int main(int argc, char** argv) {
 	//Initialize randomizer to time
-	std::srand(std::time(NULL));
+	if(argc > 1) {
+		std::srand(std::stoi(argv[1]));
+	} else {
+		std::srand(std::time(NULL));
+	}
 
 	//Program used to evaluate random expressions
 	std::ofstream out("expressions.js");
 
 	for(int i = 0; i < 100000; i++) {
-		std::string expression = generateExpression(5);
+		std::string expression = generateExpression(5, 5);
 		out << "console.log(" << expression <<  " + \",\",\"" << expression << "\");" << std::endl;
 	}
 	out.close();
